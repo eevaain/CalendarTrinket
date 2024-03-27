08d364e912b912258bfad63fa33168e747721dc3
xanderchinxyz <xanderchin@gmail.com>
2024-03-26 23:21:06 -0400

    updated everything

--- esp32code/src/main.cpp
+++ esp32code/src/main.cpp
@@ -1,22 +1,116 @@
 #include <Arduino.h>
+/*********
+  Complete project details at https://randomnerdtutorials.com
+  
+  This is an example for our Monochrome OLEDs based on SSD1306 drivers. Pick one up today in the adafruit shop! ------> http://www.adafruit.com/category/63_98
+  This example is for a 128x32 pixel display using I2C to communicate 3 pins are required to interface (two I2C and one reset).
+  Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries, with contributions from the open source community. BSD license, check license.txt for more information All text above, and the splash screen below must be included in any redistribution. 
+*********/
 
-// put function declarations here: 
-// asdasdads
-int myFunction(int, int);
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-void setup()
-{
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "XC-iPH12m";
+const char* password = "freewifi";
+
+//Your Domain name with URL path or IP address with path
+String serverName = "https://testing123-pa1y.onrender.com/events";
+
+unsigned long timerDelay = 2000;
+unsigned long lastTime = 0;
+
+#include <ArduinoJson.h>
+JsonDocument doc;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  
 }
 
-void loop()
-{
-  // put your main code here, to run repeatedly:
-}
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+      String serverPath = serverName;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+        deserializeJson(doc, payload);
+        for (JsonObject item : doc.as<JsonArray>()) {
+          String summary = item["summary"];
+          String startTime = item["start_time"];
+          String endTime = item["end_time"];
+          display.clearDisplay();
+          display.setTextSize(1);             // Normal 1:1 pixel scale
+          display.setTextColor(WHITE);        // Draw white text
+          display.setCursor(0,0);             // Start at top-left corner
+          display.println(summary);
+          display.setCursor(0,10);            // Start at top-left corner
+          display.println(startTime);
+          display.display();
+        }
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
 }
